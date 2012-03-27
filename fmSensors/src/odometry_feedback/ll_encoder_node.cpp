5a70ac78ea420a3106cf34319411674b03dd2267
Morten Larsen <mortenlarsens@gmail.com>
2012-03-27 10:27:51 +0200

    Make parameters local to node

--- fmSensors/src/odometry_feedback/ll_encoder_node.cpp
+++ fmSensors/src/odometry_feedback/ll_encoder_node.cpp
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "encoder_node");
   ros::NodeHandle nh("~");
+  ros::NodeHandle n;
 
   ROS_INFO("Started!");
 
@@ -32,21 +33,21 @@ int main(int argc, char **argv)
 
   int encoder_id,publish_rate,poll_interval_ms;
 
-  nh.param<std::string>("publisher_topic", publisher_topic, "/fmSensors/can0_tx");
-  nh.param<std::string>("subscriber_topic", subscriber_topic, "/fmSensors/can0_rx");
-  nh.param<std::string>("enc_publisher_topic", enc_publisher_topic, "/fmSensors/encoder");
+  n.param<std::string>("publisher_topic", publisher_topic, "/fmSensors/can0_tx");
+  n.param<std::string>("subscriber_topic", subscriber_topic, "/fmSensors/can0_rx");
+  n.param<std::string>("enc_publisher_topic", enc_publisher_topic, "/fmSensors/encoder");
 
-  nh.param<int>("encoder_id", encoder_id, 11);
-  nh.param<int>("publish_rate",publish_rate,10);
-  nh.param<int>("poll_interval_ms",poll_interval_ms,20);
+  n.param<int>("encoder_id", encoder_id, 11);
+  n.param<int>("publish_rate",publish_rate,10);
+  n.param<int>("poll_interval_ms",poll_interval_ms,20);
 
   ll.setID(encoder_id);
 
   ll.setPollInterval(poll_interval_ms);
 
-  ll.setCanPub(nh.advertise<fmMsgs::can> (publisher_topic.c_str(), 1));
-  ll.setEncoderPub(nh.advertise<fmMsgs::encoder> (enc_publisher_topic.c_str(), 1));
-  sub = nh.subscribe<fmMsgs::can> (subscriber_topic.c_str(), 1000, &LeineLindeEncoder::processRXEvent, &ll);
+  ll.setCanPub(nh.advertise<fmMsgs::can> (publisher_topic.c_str(), 5));
+  ll.setEncoderPub(nh.advertise<fmMsgs::encoder> (enc_publisher_topic.c_str(), 5));
+  sub = nh.subscribe<fmMsgs::can> (subscriber_topic.c_str(), 100, &LeineLindeEncoder::processRXEvent, &ll);
 
   t= nh.createTimer(ros::Duration(1.0/publish_rate),&LeineLindeEncoder::processStateMachine,&ll);
 
