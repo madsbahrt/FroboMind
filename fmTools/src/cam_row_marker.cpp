c95b6ec2add38d101d048c87fd9171c6bdfb4d30
Morten Larsen <mortenlarsens@gmail.com>
2012-03-29 10:01:23 +0200

    Added coloring to reflect quality of row message received Changed interpretation of heading and offset

--- fmTools/src/cam_row_marker.cpp
+++ fmTools/src/cam_row_marker.cpp
@@ -17,16 +17,19 @@ double line_length;
 
 void processRowEvent(const fmMsgs::claas_row_cam::ConstPtr& msg)
 {
-
 	marker.points.clear();
 	line_start.x = 0;
 	line_start.y = -(msg->offset/100.0);
 
-	line_end.x = 2;
-	line_end.y = 2 * tan(msg->heading/360.0 * 2* 3.1415);
+	line_end.x = line_length;
+	line_end.y = line_length * tan(msg->heading/360.0 * 2* 3.1415) -(msg->offset/100.0);
 
 	marker.points.push_back(line_start);
 	marker.points.push_back(line_end);
+
+	marker.color.a = 1.0;
+	marker.color.r = 1 - msg->quality/255.0;
+	marker.color.g = msg->quality/255.0;
 
 	p.publish(marker);
 }
@@ -37,14 +40,19 @@ int main(int argc,char** argv)
 	  ros::NodeHandle nh("~");
 	  ros::NodeHandle n;
 
-	  std::string sub_topic,pub_topic;
+	  std::string sub_topic,pub_topic,frame_id;
 
-	  marker.header.frame_id = "row_cam";
+
 	  marker.type = visualization_msgs::Marker::LINE_STRIP;
+	  marker.action = visualization_msgs::Marker::ADD;
+	  marker.scale.x = 0.1;
 
 	  n.param<std::string>("row_subscriber_topic",sub_topic,"/fmSensors/row");
 	  n.param<std::string>("marker_publisher_topic",pub_topic,"/fmTools/row_marker");
+	  n.param<std::string>("frame_id",frame_id,"cam_link");
+	  n.param<double>("line_length",line_length,2);
 
+	  marker.header.frame_id = frame_id;
 
 	  p = nh.advertise<visualization_msgs::Marker>(pub_topic.c_str(),10);
 	  s = nh.subscribe(sub_topic.c_str(),10,processRowEvent);
