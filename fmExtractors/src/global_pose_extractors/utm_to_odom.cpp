9a7946344546bc968b19e38f705bd0e27ea60b62
Morten Larsen <mortenlarsens@gmail.com>
2012-04-29 13:36:22 +0200

    Add option to run in either UTm or locally reference coordinate system

--- fmExtractors/src/global_pose_extractors/utm_to_odom.cpp
+++ fmExtractors/src/global_pose_extractors/utm_to_odom.cpp
@@ -18,11 +18,11 @@ fmMsgs::gps_state prev_msg;
 nav_msgs::Odometry odom;
 
 
-
 geometry_msgs::TransformStamped odom_trans;
 std::string frame_id,tf_child_frame_id,tf_frame_id;
 
 
+bool publish_relative = false;
 bool utm_settled=false;
 int utm_settled_count = 0;
 
@@ -44,19 +44,20 @@ void utmCallback(const fmMsgs::gps_state::ConstPtr& msg)
 	else if(utm_settled)
 	{
 
-		double dx,dy;
-		double dt;
-
-		dx = (msg->utm_n - ref_fix.utm_n) - (prev_msg.utm_n - ref_fix.utm_n);
-		dy = (ref_fix.utm_e - msg->utm_e ) - (ref_fix.utm_e - prev_msg.utm_e);
-
-		dt = (msg->header.stamp - prev_msg.header.stamp).toSec();
-
 		odom.header.stamp = ros::Time::now();
 		odom.header.frame_id =frame_id;
-		//odom.pose.pose.position.x = msg->utm_n - ref_fix.utm_n;
-		odom.pose.pose.position.x = -(ref_fix.utm_e - msg->utm_e);
-		odom.pose.pose.position.y = -(ref_fix.utm_n - msg->utm_n);
+
+		if(publish_relative){
+			odom.pose.pose.position.x = -(ref_fix.utm_e - msg->utm_e);
+			odom.pose.pose.position.y = -(ref_fix.utm_n - msg->utm_n);
+		}
+		else
+		{
+			// ENU
+			odom.pose.pose.position.x =msg->utm_e;
+			odom.pose.pose.position.y =msg->utm_n;
+		}
+
 		odom.pose.pose.position.z = 0;
 
 		odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
@@ -65,34 +66,11 @@ void utmCallback(const fmMsgs::gps_state::ConstPtr& msg)
 
 		odom.pose.covariance[7] = msg->hdop * msg->hdop* 0.01;
 
-		odom.pose.covariance[14] = 0.00001;
+		odom.pose.covariance[14] = 999999;
 
 		odom.pose.covariance[21] = 999999;
 		odom.pose.covariance[28] = 999999;
 		odom.pose.covariance[35] = 999999;
-
-		odom.twist.twist.linear.x = sqrt(dx*dx + dy*dy)/dt;
-		odom.twist.twist.linear.y = 0.0;
-
-		odom_trans.header.stamp = ros::Time::now();
-		odom_trans.header.frame_id = tf_frame_id;
-		odom_trans.child_frame_id = tf_child_frame_id;
-		odom_trans.transform.translation.x =  msg->utm_n - ref_fix.utm_n;
-		odom_trans.transform.translation.y = ref_fix.utm_e - msg->utm_e;
-		odom_trans.transform.translation.z = msg->alt;
-
-		double yaw = atan2(dy,dx);
-
-		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(yaw);
-
-		odom_trans.transform.rotation = odom_quat;
-
-		/*odom_trans.transform.rotation.w = 1;
-		odom_trans.transform.rotation.x = 0;
-		odom_trans.transform.rotation.y = 0;
-		odom_trans.transform.rotation.z = 0;*/
-
-		//odom_broadcaster.sendTransform(odom_trans);
 
 		odom_pub.publish(odom);
 
@@ -115,8 +93,7 @@ int main(int argc, char **argv)
 	n.param<std::string>("publish_topic_id", publish_topic_id,
 			"/fmExtractors/gps_odom");
 	n.param<std::string>("odom_frame_id",frame_id,"base_footprint");
-	n.param<std::string>("tf_frame_id",tf_frame_id,"world");
-	n.param<std::string>("tf_child_frame_id",tf_child_frame_id,"tf_gps_odom");
+	n.param<bool>("publish_relative_coordinates",publish_relative,false);
 
 	ros::Subscriber sub = n.subscribe(subscribe_topic_id, 10, utmCallback);
 	odom_pub = n.advertise<nav_msgs::Odometry>(publish_topic_id, 1);
