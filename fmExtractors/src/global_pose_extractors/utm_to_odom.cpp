4a05b5ca5996f42e1552587f6d91f348c1e9c7d2
Morten Larsen <mortenlarsens@gmail.com>
2012-06-03 13:08:45 +0200

    Added new heading calculator

--- fmExtractors/src/global_pose_extractors/utm_to_odom.cpp
+++ fmExtractors/src/global_pose_extractors/utm_to_odom.cpp
@@ -10,12 +10,24 @@
 #include <geometry_msgs/TransformStamped.h>
 #include <tf/transform_broadcaster.h>
 
+#include <boost/circular_buffer.hpp>
+#include <math.h>
+
 ros::Publisher odom_pub;
 
 fmMsgs::gps_state ref_fix;
 fmMsgs::gps_state prev_msg;
 
+
 nav_msgs::Odometry odom;
+
+typedef struct
+{
+	double x;
+	double y;
+}gps_points_t;
+
+boost::circular_buffer<gps_points_t> gps_points_buffer(25);
 
 double gps_variance;
 
@@ -27,6 +39,36 @@ bool publish_relative = false;
 bool utm_settled=false;
 int utm_settled_count = 0;
 int utm_settled_count_top = 0;
+
+double heading_threshold;
+double heading_variance;
+
+
+bool calculate_heading(double& heading)
+{
+	bool ret = false;
+
+
+
+	if(gps_points_buffer.capacity() == gps_points_buffer.size())
+	{
+		gps_points_t p_comp = gps_points_buffer[0];
+
+		for(size_t i=1; i < gps_points_buffer.size();i++)
+		{
+			gps_points_t p_cur = gps_points_buffer[i];
+			double dist = sqrt(pow(p_comp.x - p_cur.x,2) + pow(p_comp.y - p_cur.y,2));
+			if(dist > heading_threshold)
+			{
+				heading = atan2(p_comp.y - p_cur.y,p_comp.x - p_cur.x);
+				ret = true;
+				break;
+			}
+		}
+	}
+
+	return ret;
+}
 
 void utmCallback(const fmMsgs::gps_state::ConstPtr& msg)
 {
@@ -46,6 +88,12 @@ void utmCallback(const fmMsgs::gps_state::ConstPtr& msg)
 	else if(utm_settled)
 	{
 
+		gps_points_t p;
+		p.x = msg->utm_e;
+		p.y = msg->utm_n;
+
+		gps_points_buffer.push_front(p);
+
 		odom.header.stamp = ros::Time::now();
 		odom.header.frame_id =frame_id;
 		odom.child_frame_id = child_frame_id;
@@ -63,15 +111,19 @@ void utmCallback(const fmMsgs::gps_state::ConstPtr& msg)
 
 		odom.pose.pose.position.z = 0;
 
-		double yaw,dx,dy;
+		static double yaw=0;
 
-		dx = msg->utm_e - prev_msg.utm_e;
-		dy = msg->utm_n - prev_msg.utm_n;
+		if(calculate_heading(yaw))
+		{
+			odom.pose.covariance[35] = heading_variance;
+		}
+		else
+		{
+			odom.pose.covariance[35] = 999999;
 
-		yaw = atan2(dy,dx);
+		}
 
 		odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
-
 		odom.pose.covariance[0] = msg->hdop * gps_variance;
 
 		odom.pose.covariance[7] = msg->hdop * gps_variance;
@@ -80,7 +132,7 @@ void utmCallback(const fmMsgs::gps_state::ConstPtr& msg)
 
 		odom.pose.covariance[21] = 999999;
 		odom.pose.covariance[28] = 999999;
-		odom.pose.covariance[35] = msg->hdop * gps_variance;
+
 
 		odom_pub.publish(odom);
 
@@ -108,6 +160,8 @@ int main(int argc, char **argv)
 	n.param<int>("receive_n_before_publish",utm_settled_count_top,3);
 	n.param<double>("gps_variance",gps_variance,1);
 
+	n.param<double>("gps_heading_threshold",heading_threshold,1);
+	n.param<double>("gps_heading_variance",heading_variance,0.01);
 	ros::Subscriber sub = n.subscribe(subscribe_topic_id, 10, utmCallback);
 	odom_pub = n.advertise<nav_msgs::Odometry>(publish_topic_id, 1);
 
